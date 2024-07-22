012411df1fe021d5fd4fc068e990815b826663d7
Jason Abbott <jason@toba.io>
2024-07-22 15:10:09 -0600

    create xcode project

---
+++ GRDBQuery/GRDBQuery/GRDBQuery.h
@@ -0,0 +1,18 @@
+//
+//  GRDBQuery.h
+//  GRDBQuery
+//
+//  Created by Jason Abbott on 7/22/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GRDBQuery.
+FOUNDATION_EXPORT double GRDBQueryVersionNumber;
+
+//! Project version string for GRDBQuery.
+FOUNDATION_EXPORT const unsigned char GRDBQueryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GRDBQuery/PublicHeader.h>
+
+
