2caa88dee1165d631a17da9782d5568a611a0005
Jason Abbott <jason@toba.io>
2024-07-22 14:38:26 -0600

    Remove GRDB dependency

--- Package.swift
+++ Package.swift
@@ -17,23 +17,23 @@ let package = Package(
             name: "GRDBQuery",
             targets: ["GRDBQuery"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/groue/GRDB.swift.git", from: "6.4.0"),
-    ],
+//    dependencies: [
+//        // Dependencies declare other packages that this package depends on.
+//        .package(url: "https://github.com/groue/GRDB.swift.git", from: "6.4.0"),
+//    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "GRDBQuery",
             dependencies: [
-                .product(name: "GRDB", package: "GRDB.swift"),
+                // .product(name: "GRDB", package: "GRDB.swift"),
             ]),
         .testTarget(
             name: "GRDBQueryTests",
             dependencies: [
                 "GRDBQuery",
-                .product(name: "GRDB", package: "GRDB.swift"),
+                // .product(name: "GRDB", package: "GRDB.swift"),
             ]),
     ]
 )
